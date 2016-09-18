f0d95a013afe08bbc5cc916927e3b04b5eec8259
Jeffrey Pfau <jeffrey@endrift.com>
2016-09-18 16:05:20 -0700

    Update Qt configure script

--- buildscripts/configure-qt.sh
+++ buildscripts/configure-qt.sh
@@ -9,6 +9,20 @@ fi
 BASEDIR=$(dirname $0)
 . $BASEDIR/identify-toolchain.sh
 OS=$(identify_os $CC)
+COMPILER=$(identify_compiler $CXX)
+
+export QMAKE_CXXFLAGS=$CXXFLAGS
+
+unset CC
+unset CXX
+unset AR
+unset RANLIB
+unset STRIP
+unset CPPFLAGS
+unset CFLAGS
+unset CXXFLAGS
+unset LDFLAGS
+
 case $OS in
 FreeBSD*)
 	OS=freebsd
@@ -23,11 +37,10 @@ Windows*)
 	OS=win32
 	;;
 esac
-COMPILER=$(identify_compiler $CXX)
 
 case `uname` in
 FreeBSD)
-	HOST=freebsd-g++
+	HOST=freebsd-clang
 	;;
 Linux)
 	OS=linux-g++
@@ -37,22 +50,21 @@ Darwin)
 	;;
 esac
 
-unset CC
-unset CXX
-unset AR
-unset RANLIB
-unset STRIP
-unset CPPFLAGS
-unset CFLAGS
-unset CXXFLAGS
-unset LDFLAGS
-
 ./configure -prefix $ROOT -opensource -confirm-license -xplatform $OS-$COMPILER \
 	-device-option CROSS_COMPILE=$CROSS_COMPILE -release -platform $HOST \
 	-skip 3d -skip activeqt -skip canvas3d -skip connectivity -skip declarative \
 	-skip doc -skip docgallery -skip enginio -skip feedback -skip graphicaleffects \
-	-skip location -skip pim -skip qa -skip quick1 -skip quickcontrols -skip repotools \
-	-skip script -skip sensors -skip serialport -skip svg -skip systems -skip tools \
-	-skip wayland -skip webchannel -skip webengine -skip webkit -skip webkit-examples \
-	-skip websockets -skip xmlpatterns \
-	-no-icu -nomake examples -nomake tools -qt-harfbuzz -v
+	-skip imageformats -skip location -skip pim -skip qa -skip quick1 \
+	-skip quickcontrols -skip repotools -skip script -skip sensors \
+	-skip serialport -skip svg -skip systems -skip tools -skip wayland \
+	-skip webchannel -skip webengine -skip webkit -skip webkit-examples \
+	-skip websockets -skip xmlpatterns -skip multimedia -nomake examples \
+	-nomake tools -nomake tests -no-icu -no-compile-examples -no-gif -no-sql-odbc \
+	-no-feature-printer -no-feature-printpreviewwidget -no-feature-printdialog \
+	-no-feature-ftp -no-feature-http -no-feature-udpsocket -no-feature-socks5 \
+	-no-feature-networkproxy -no-feature-networkdiskcache \
+	-no-feature-bearermanagement -no-feature-fontcombobox -no-feature-mdiarea \
+	-no-feature-colordialog -no-feature-fontdialog -no-feature-wizard \
+	-no-feature-imageformat-ppm -no-feature-imageformat-xbm -no-feature-pdf \
+	-no-harfbuzz -no-openssl -I $ROOT/include -L $ROOT/lib -v -static -ltcg \
+	-c++std c++14
