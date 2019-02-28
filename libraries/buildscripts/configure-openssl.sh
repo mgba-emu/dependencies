28b04c5db7dc533c4d3332d1dabd5757846d0da8
Vicki Pfau <vi@endrift.com>
2019-02-27 18:32:27 -0800

    libraries: Add openssl

---
+++ libraries/buildscripts/configure-openssl.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+if [ -z "$CROSS_COMPILE" ]; then
+	export CROSS_COMPILE=$1
+fi
+if [ -z "$ROOT" ]; then
+	export ROOT=$2
+fi
+
+BASEDIR=$(dirname $0)
+. $BASEDIR/identify-toolchain.sh
+OS=$(identify_os $CC)
+
+unset AR
+unset CC
+unset RANLIB
+unset STRIP
+
+case $OS in
+Windows64)
+	SYSTEM=MINGW64
+	MACHINE=x86_64
+	export SYSTEM
+	export MACHINE
+	;;
+Windows*)
+	SYSTEM=MINGW32
+	MACHINE=i686
+	export SYSTEM
+	export MACHINE
+	;;
+esac
+
+CFLAGS=-I$ROOT/include
+LDFLAGS=-L$ROOT/lib
+export CFLAGS
+export LDFLAGS
+
+./config --prefix=$ROOT --openssldir=$ROOT no-shared no-ssl2 no-ssl3 no-idea
