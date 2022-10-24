0aef682793ab9b2fb393e411b43e1275904f5598
Vicki Pfau <vi@endrift.com>
2022-10-24 06:12:50 -0700

    libraries: Add curl

---
+++ libraries/buildscripts/configure-curl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ -z "$CMAKE_TOOLCHAIN_FILE" ]; then
+	export CMAKE_TOOLCHAIN_FILE=$1
+fi
+if [ -z "$ROOT" ]; then
+	export ROOT=$2
+fi
+
+BASEDIR=$(dirname $0)
+. $BASEDIR/identify-toolchain.sh
+OS=$(identify_os $CC)
+CMAKE_EXTRA=""
+case $OS in
+OSX*)
+	ARCH=${HOST%%-*}
+	if [ -z "$ARCH" ]; then
+		ARCH=$(arch)
+	fi
+	if [ $ARCH == aarch64 ]; then
+		ARCH=arm64
+	else
+		CMAKE_EXTRA="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.13"
+	fi
+	CMAKE_EXTRA="$CMAKE_EXTRA -DCMAKE_OSX_ARCHITECTURES=$ARCH"
+	;;
+esac
+
+mkdir -p build
+cd build && cmake .. \
+	-DCMAKE_INSTALL_PREFIX="$ROOT" \
+	-DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" \
+	$CMAKE_EXTRA
